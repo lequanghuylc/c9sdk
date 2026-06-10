e0ab16edf758e012c4fe50678392c37e459f9d85
Apple <Apple@192.168.102.2>
2026-06-10 16:50:14 +0700

    Add --platform linux/amd64 to docker build in run.sh

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ echo "Access the IDE at: http://localhost:8080/$SUB_PATH/"
 
 # Build the Docker image with C9_SUB_PATH
 cd c9sdk-pm2-nginx
-docker build --build-arg C9_SUB_PATH="$SUB_PATH" -t lequanghuylc/c9sdk-pm2-ubuntu:latest .
+docker build --platform linux/amd64 --build-arg C9_SUB_PATH="$SUB_PATH" -t lequanghuylc/c9sdk-pm2-ubuntu:latest .
 
 # Run the container
 docker run --platform linux/amd64 --rm -p 8080:8080 -p 3399:3399 lequanghuylc/c9sdk-pm2-ubuntu:latest
